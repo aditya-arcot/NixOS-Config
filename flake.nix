dfbcf8a9d01f4313477232348e6e0625000c1105
aditya-arcot <dev@aarcot.com>
2026-02-14 14:23:28 -0600

    add initial files

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    grub2-themes.url = "github:vinceliuice/grub2-themes";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs,  grub2-themes, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          grub2-themes.nixosModules.default
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.adityaarcot = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
+
---
+++ home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "adityaarcot";
+  home.homeDirectory = "/home/adityaarcot";
+
+  home.stateVersion = "25.11";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+  ];
+
+  programs.git = {
+    enable = true;
+    settings.user.name = "aditya-arcot";
+    settings.user.email = "dev@aarcot.com";
+  };
+
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
+
+  services.ssh-agent.enable = true;
+
+  programs.zsh.enable = true;
+}
