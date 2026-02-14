75f9ebdbac53b93fa5502c5faa89360b52d65860
aditya-arcot <dev@aarcot.com>
2026-02-14 15:33:18 -0600

    format

--- flake.nix
+++ flake.nix
@@ -1,37 +1,44 @@
 {
-  description = "NixOS system";
+    description = "NixOS system";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    grub2-themes.url = "github:vinceliuice/grub2-themes";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        grub2-themes.url = "github:vinceliuice/grub2-themes";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        vscode-server.url = "github:nix-community/nixos-vscode-server";
     };
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
-  };
 
-  outputs = inputs@{ nixpkgs,  grub2-themes, home-manager, vscode-server, ... }: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hardware-configuration.nix
-          ./configuration.nix
-          grub2-themes.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {
-              vscode-server = inputs.vscode-server;
+    outputs =
+        inputs@{
+            nixpkgs,
+            grub2-themes,
+            home-manager,
+            vscode-server,
+            ...
+        }:
+        {
+            nixosConfigurations = {
+                nixos = nixpkgs.lib.nixosSystem {
+                    system = "x86_64-linux";
+                    specialArgs = { inherit inputs; };
+                    modules = [
+                        ./hardware-configuration.nix
+                        ./configuration.nix
+                        grub2-themes.nixosModules.default
+                        home-manager.nixosModules.home-manager
+                        {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.extraSpecialArgs = {
+                                vscode-server = inputs.vscode-server;
+                            };
+                            home-manager.users.adityaarcot = import ./home.nix;
+                        }
+                    ];
+                };
             };
-            home-manager.users.adityaarcot = import ./home.nix;
-          }
-        ];
-      };
-    };
-  };
+        };
 }
-
--- home.nix
+++ home.nix
@@ -1,42 +1,47 @@
-{ config, pkgs, vscode-server, ... }:
+{
+    config,
+    pkgs,
+    vscode-server,
+    ...
+}:
 
 {
-  imports = [
-    "${vscode-server}/modules/vscode-server/home.nix"
-  ];
+    imports = [
+        "${vscode-server}/modules/vscode-server/home.nix"
+    ];
 
-  home.username = "adityaarcot";
-  home.homeDirectory = "/home/adityaarcot";
+    home.username = "adityaarcot";
+    home.homeDirectory = "/home/adityaarcot";
 
-  home.stateVersion = "25.11";
+    home.stateVersion = "25.11";
 
-  programs.home-manager.enable = true;
+    programs.home-manager.enable = true;
 
-  home.packages = with pkgs; [
-    nixfmt
-  ];
+    home.packages = with pkgs; [
+        nixfmt
+    ];
 
-  programs.git = {
-    enable = true;
-    settings.user.name = "aditya-arcot";
-    settings.user.email = "dev@aarcot.com";
-  };
-
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-    matchBlocks = {
-      "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/id_ed25519";
-      };
+    programs.git = {
+        enable = true;
+        settings.user.name = "aditya-arcot";
+        settings.user.email = "dev@aarcot.com";
     };
-  };
 
-  services.ssh-agent.enable = true;
+    programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+        matchBlocks = {
+            "github.com" = {
+                hostname = "github.com";
+                user = "git";
+                identityFile = "~/.ssh/id_ed25519";
+            };
+        };
+    };
 
-  services.vscode-server.enable = true;
+    services.ssh-agent.enable = true;
 
-  programs.zsh.enable = true;
+    services.vscode-server.enable = true;
+
+    programs.zsh.enable = true;
 }
